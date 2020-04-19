394ff3002b3ff51404cb46447f7d3623d8e6b2df
Ravi-TStark <58636984+Ravi-TStark@users.noreply.github.com>
2020-04-19 21:17:24 +0530

    19-04-2020 21:16
    Wrote a Lousy Algorithm to search for Installed Applications using Startmenu Shortcuts. But Can Improvise it.

--- Star Gazer/MainWindow.xaml.cs
+++ Star Gazer/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading;
@@ -10,10 +15,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace Star_Gazer
 {
@@ -26,6 +33,7 @@ namespace Star_Gazer
         {
             InitializeComponent();
         }
+        public List<string> itms = new List<string>();
         protected override void OnPreviewKeyDown(System.Windows.Input.KeyEventArgs e)
         {
             base.OnPreviewKeyDown(e);
@@ -63,6 +71,18 @@ namespace Star_Gazer
                 listBox.Focus();
             }
         }
+        public static ImageSource GetIcon(string fileName)
+        {
+            try
+            {
+                Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(fileName);
+                return System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(icon.Handle, new Int32Rect(0, 0, icon.Width, icon.Height), BitmapSizeOptions.FromEmptyOptions());
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -72,19 +92,118 @@ namespace Star_Gazer
             }
             else
             {
-                listBox.Visibility = Visibility.Visible;
                 listBox.Items.Clear();
-                if(listBox.Items.Count == 0)
+                itms.Clear();
+                /*string uninstallKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+                using (RegistryKey rk = Registry.LocalMachine.OpenSubKey(uninstallKey))
+                {
+                    foreach (string skName in rk.GetSubKeyNames())
+                    {
+                        using (RegistryKey sk = rk.OpenSubKey(skName))
+                        {
+                            var displayName = sk.GetValue("DisplayName");
+                            var path = sk.GetValue("DisplayIcon");
+                                SearchListItem item;
+                            if (displayName != null)
+                            {
+                                if (displayName.ToString().ToLower().Contains(textBox.Text.ToLower()))
+                                {
+                                    item = new SearchListItem();
+                                    item.Width = listBox.Width - 35;
+                                    item.label.Content = displayName.ToString();
+                                    item.label2.Visibility = Visibility.Hidden;
+                                    if (path != null)
+                                    {
+                                        item.label2.Visibility = Visibility.Visible;
+                                        item.label2.Content = Directory.GetParent(path.ToString().Replace("\"",""));
+                                        item.apppath = path.ToString();
+                                        if(item.apppath.EndsWith(",0"))
+                                        {
+                                            item.apppath = path.ToString().Replace(",0", "");
+                                        }
+                                    }
+                                    if (System.IO.Path.GetExtension(item.apppath) == ".exe")
+                                    {
+                                        item.image.Source = GetIcon(item.apppath);
+                                        listBox.Items.Add(item);
+                                        itms.Add(item.apppath);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+                //
+                string uninstallKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";
+                using (RegistryKey rk = Registry.LocalMachine.OpenSubKey(uninstallKey))
+                {
+                    foreach (string skName in rk.GetSubKeyNames())
+                    {
+                        using (RegistryKey sk = rk.OpenSubKey(skName))
+                        {
+                            var displayName = sk.GetValue("");
+                            var path = sk.GetValue("Path");
+                            SearchListItem item;
+                            if (displayName != null)
+                            {
+                                if (displayName.ToString().ToLower().Contains(textBox.Text.ToLower()))
+                                {
+                                    item = new SearchListItem();
+                                    item.Width = listBox.Width - 35;
+                                    item.label.Content = GetAppName(displayName.ToString().Replace("\"", ""));
+                                    item.label2.Visibility = Visibility.Hidden;
+                                    if (path != null)
+                                    {
+                                        item.label2.Visibility = Visibility.Visible;
+                                        item.label2.Content = path.ToString();
+                                        item.apppath = displayName.ToString().Replace("\"", "");
+                                    }
+                                    else
+                                    {
+                                        item.label2.Visibility = Visibility.Visible;
+                                        item.label2.Content = Directory.GetParent(displayName.ToString().Replace("\"", ""));
+                                        item.apppath = displayName.ToString().Replace("\"", "");
+                                    }
+                                    item.image.Source = GetIcon(item.apppath);
+                                    if (!itms.Contains(item.apppath) && File.Exists(item.apppath))
+                                    {
+                                        //itms.Add(item.apppath);
+                                        //listBox.Items.Add(item);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }*/
+                //
+                foreach (string fil in Directory.EnumerateFiles("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\", "*.lnk", SearchOption.AllDirectories))
                 {
                     var item = new SearchListItem();
-                    item.Width = listBox.Width-15;
-                    item.Height = 64;
-                    listBox.Items.Add(item);
-                    item = new SearchListItem();
-                    item.Width = listBox.Width - 15;
-                    item.Height = 64;
-                    listBox.Items.Add(item);
+                    item.Width = listBox.Width - 35;
+                    item.label.Content = System.IO.Path.GetFileNameWithoutExtension(fil);
+                    item.label2.Visibility = Visibility.Visible;
+                    item.apppath = fil;
+                    item.label2.Content = Directory.GetParent(item.apppath);
+                    item.image.Source = GetIcon(item.apppath);
+                    if (item.label.Content.ToString().ToLower().Contains(textBox.Text.ToLower()) || System.IO.Path.GetFileNameWithoutExtension(item.apppath).ToLower().Contains(textBox.Text.ToLower()))
+                    {
+                        listBox.Items.Add(item);
+                    }
                 }
+                //
+                if (listBox.Items.Count == 0)
+                {
+                    SearchListItem item;
+                    item = new SearchListItem();
+                    item.Width = listBox.Width - 35;
+                    item.label.Content = "No match found";
+                    item.label2.Visibility = Visibility.Hidden;
+                    listBox.Items.Add(item);
+
+                }
+                listBox.Visibility = Visibility.Visible;
+                //
             }
         }
 
@@ -92,6 +211,99 @@ namespace Star_Gazer
         {
             sGazer.MaxHeight = SystemParameters.WorkArea.Height;
             sGazer.MaxWidth = SystemParameters.WorkArea.Width;
+        }
+        
+        private void listBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SearchListItem tmp = (SearchListItem)listBox.SelectedItem;
+                tmp.Click();
+            }
+        }
+
+        public string GetAppName(string path)
+        {
+            if (File.Exists(path))
+            {
+                FileVersionInfo inf = FileVersionInfo.GetVersionInfo(path);
+                return inf.ProductName;
+            }
+            return "";
+        }
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                SearchListItem tmp = (SearchListItem)listBox.Items[0];
+                if(tmp.label.Content.ToString() != "No match found")
+                {
+                    tmp.Click();
+                }
+                else
+                {
+                    SystemSounds.Exclamation.Play();
+                    this.Close();
+                }
+            }
+        }
+        private string GetShortcutTarget(string file)
+        {
+            try
+            {
+                if (System.IO.Path.GetExtension(file).ToLower() != ".lnk")
+                {
+                    throw new Exception("Supplied file must be a .LNK file");
+                }
+
+                FileStream fileStream = File.Open(file, FileMode.Open, FileAccess.Read);
+                using (System.IO.BinaryReader fileReader = new BinaryReader(fileStream))
+                {
+                    fileStream.Seek(0x14, SeekOrigin.Begin);     // Seek to flags
+                    uint flags = fileReader.ReadUInt32();        // Read flags
+                    if ((flags & 1) == 1)
+                    {                      // Bit 1 set means we have to
+                                           // skip the shell item ID list
+                        fileStream.Seek(0x4c, SeekOrigin.Begin); // Seek to the end of the header
+                        uint offset = fileReader.ReadUInt16();   // Read the length of the Shell item ID list
+                        fileStream.Seek(offset, SeekOrigin.Current); // Seek past it (to the file locator info)
+                    }
+
+                    long fileInfoStartsAt = fileStream.Position; // Store the offset where the file info
+                                                                 // structure begins
+                    uint totalStructLength = fileReader.ReadUInt32(); // read the length of the whole struct
+                    fileStream.Seek(0xc, SeekOrigin.Current); // seek to offset to base pathname
+                    uint fileOffset = fileReader.ReadUInt32(); // read offset to base pathname
+                                                               // the offset is from the beginning of the file info struct (fileInfoStartsAt)
+                    fileStream.Seek((fileInfoStartsAt + fileOffset), SeekOrigin.Begin); // Seek to beginning of
+                                                                                        // base pathname (target)
+                    long pathLength = (totalStructLength + fileInfoStartsAt) - fileStream.Position - 2; // read
+                                                                                                        // the base pathname. I don't need the 2 terminating nulls.
+                    char[] linkTarget = fileReader.ReadChars((int)pathLength); // should be unicode safe
+                    var link = new string(linkTarget);
+
+                    int begin = link.IndexOf("\0\0");
+                    if (begin > -1)
+                    {
+                        int end = link.IndexOf("\\\\", begin + 2) + 2;
+                        end = link.IndexOf('\0', end) + 1;
+
+                        string firstPart = link.Substring(0, begin);
+                        string secondPart = link.Substring(end);
+
+                        return firstPart + secondPart;
+                    }
+                    else
+                    {
+                        return link;
+                    }
+                }
+            }
+            catch
+            {
+                return "";
+            }
         }
     }
 }
--- Star Gazer/SearchListItem.xaml.cs
+++ Star Gazer/SearchListItem.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,73 @@ namespace Star_Gazer
     /// </summary>
     public partial class SearchListItem : UserControl
     {
+        public string apppath;
+        public bool isApp = false;
+
         public SearchListItem()
         {
             InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                if (!isApp)
+                {
+                    if (File.Exists(apppath))
+                    {
+                        try
+                        {
+                            Process.Start(apppath);
+                            Window parentWindow = Window.GetWindow(this);
+                            parentWindow.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void label2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                try
+                {
+                    //Process.Start(label2.Content.ToString());
+                }
+                catch(Exception ex)
+                {
+                    
+                }
+                Window parentWindow = Window.GetWindow(this); 
+                parentWindow.Close();
+            }
+        }
+
+        public void Click()
+        {
+            if (File.Exists(apppath))
+            {
+                try
+                {
+                    Process.Start(apppath);
+                    Window parentWindow = Window.GetWindow(this);
+                    parentWindow.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
     }
 }
